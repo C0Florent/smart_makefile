4ffff325a518eed51bb1350b1f339a6fc609b934
C0Florent <florent.charpentier@epitech.eu>
2024-05-30 18:06:19 +0200

    :sparkles: Added improved version of demo Makefile

---
+++ smart_base/defs.h
@@ -0,0 +1,9 @@
+
+#ifndef MY_DEFS
+    #define MY_DEFS
+
+    #include "defs_internal.h"
+
+    #define MY_RETURN_VALUE 41
+
+#endif /* MY_DEFS */
---
+++ smart_base/defs_internal.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** smart_makefile
+** File description:
+** defs_internal
+*/
+
+#ifndef DEFS_INTERNAL_H_
+    #define DEFS_INTERNAL_H_
+
+
+#endif /* !DEFS_INTERNAL_H_ */
